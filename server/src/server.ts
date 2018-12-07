756e422c8c62522fd355d9b9b967080e0847491b
benjaminJ <kamikillerto@gmail.com>
2018-12-07 15:16:53 +0100

    feat: React to .htmllintrc changes
    All files already linted will be linted again

--- server/src/server.ts
+++ server/src/server.ts
@@ -195,8 +195,10 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
   // Send the computed diagnostics to VSCode.
   connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
-
 connection.onDidChangeWatchedFiles(_change => {
+  // changes globalConfig
+  // need to load config once before ^^
+  documents.all().forEach(file => validateTextDocument(file));
   // Monitored files have change in VSCode
   connection.console.log('We received an file change event');
 });
@@ -207,35 +209,36 @@ connection.onCompletion(
     // The pass parameter contains the position of the text document in
     // which code complete got requested. For the example we ignore this
     // info and always provide the same completion items.
+    // Return completion of htmllint rules
     return [
-      {
-        label: 'TypeScript',
-        kind: CompletionItemKind.Text,
-        data: 1
-      },
-      {
-        label: 'JavaScript',
-        kind: CompletionItemKind.Text,
-        data: 2
-      }
+      // {
+      //   label: 'TypeScript',
+      //   kind: CompletionItemKind.Text,
+      //   data: 1
+      // },
+      // {
+      //   label: 'JavaScript',
+      //   kind: CompletionItemKind.Text,
+      //   data: 2
+      // }
     ];
   }
 );
 
 // This handler resolve additional information for the item selected in
 // the completion list.
-connection.onCompletionResolve(
-  (item: CompletionItem): CompletionItem => {
-    if (item.data === 1) {
-      (item.detail = 'TypeScript details'),
-        (item.documentation = 'TypeScript documentation');
-    } else if (item.data === 2) {
-      (item.detail = 'JavaScript details'),
-        (item.documentation = 'JavaScript documentation');
-    }
-    return item;
-  }
-);
+// connection.onCompletionResolve(
+//   (item: CompletionItem): CompletionItem => {
+//     if (item.data === 1) {
+//       (item.detail = 'TypeScript details'),
+//         (item.documentation = 'TypeScript documentation');
+//     } else if (item.data === 2) {
+//       (item.detail = 'JavaScript details'),
+//         (item.documentation = 'JavaScript documentation');
+//     }
+//     return item;
+//   }
+// );
 
 connection.onDidOpenTextDocument((params) => {
   debugger
